14b7ad7ebd7336232420d86426175e796852c565
skylerspark <44554976+skylerspark@users.noreply.github.com>
2019-06-26 12:23:06 -0400

    hid debug page
    added konami code to use the debug page.

--- js/index.js
+++ js/index.js
@@ -8,3 +8,37 @@ function link(linktex) {
   window.location.assign(linktex);
   console.log("Redirecting to " + linktex);
 }
+
+window.onload = function() {
+  document.getElementById('a7').style.display = 'none';
+}
+
+var allowedKeys = {
+  32: 'spc',
+  37: 'left',
+  38: 'up',
+  39: 'right',
+  40: 'down',
+  65: 'a',
+  66: 'b'
+};
+var kCode = ['spc','up','down','spc'];
+var kCodePos = 0;
+
+document.addEventListener('keydown', function(e) {
+  var key = allowedKeys[e.keyCode];
+  var requiredKey = kCode[kCodePos];
+  if (key == requiredKey) {
+    kCodePos++;
+    if (kCodePos == kCode.length) {
+      funcActive();
+      kCodePos = 0;
+    }
+  } else {
+    kCodePos = 0;
+  }
+});
+
+function funcActive() {
+  document.getElementById('a7').style.display = 'initial';
+}
