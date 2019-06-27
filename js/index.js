dfc018f90232a5bfbe5f8fabc5907e8715125b50
skylerspark <44554976+skylerspark@users.noreply.github.com>
2019-06-27 15:58:02 -0400

    Removed All debug stuff. FiNALLY

--- js/index.js
+++ js/index.js
@@ -16,54 +16,6 @@ function link(linktex) {
 }
 
 
-// Hide debug element on page load
-
-window.onload = function() {
-  document.getElementById('a7').style.display = 'none';
-}
-
-
-// Konami Code thing to show Debug Link
-
-var allowedKeys = {
-  32: '1',
-  37: '2',
-  38: '3',
-  39: '4',
-  40: '5',
-  65: '6',
-  69: '7',
-  78: '8',
-  89: '9'
-};
-var kCode = ['1','3','4','5','6','9','1','7','2','8','6','9','4','5','1'];
-var kCodePos = 0;
-
-document.addEventListener('keydown', function(e) {
-  var key = allowedKeys[e.keyCode];
-  var requiredKey = kCode[kCodePos];
-  if (key == requiredKey) {
-    kCodePos++;
-    if (kCodePos == kCode.length) {
-      console.log("Keybind Correct");
-      funcActive();
-      kCodePos = 0;
-    }
-  } else {
-    kCodePos = 0;
-  }
-});
-
-
-// Show Hidden Debug Link
-
-function funcActive() {
-  console.log('function activated');
-  document.getElementById('a7').style.display = '';
-  document.getElementById('a7').style.visibility = 'visible!important';
-}
-
-
 // IP Grabber (Render a Public IP)
 
 fetch('https://api.ipify.org/?format=json')
