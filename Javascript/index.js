39bc7513a898c34852ed2753193ab001caf273c6
skylerspark <44554976+skylerspark@users.noreply.github.com>
2019-10-21 17:05:45 -0400

    Big Update!

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,7 +1,12 @@
+// STUB START
+
 function sleep(s) {
     return new Promise(r => setTimeout(r, s));
 }
 
+// STUB END
+
+// TODO: 
 async function link(link) {
     document.getElementById("contentCover").style.background = "white";
     await sleep(500);
