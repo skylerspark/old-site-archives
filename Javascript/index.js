82346d645e8a0eb10782c5affb320e822ca606fa
skylerspark <44554976+skylerspark@users.noreply.github.com>
2019-10-21 18:42:30 -0400

    Lots of Updates!

--- Javascript/index.js
+++ Javascript/index.js
@@ -6,11 +6,28 @@ function sleep(s) {
 
 // STUB END
 
-// TODO: 
+// TODO
 async function link(link) {
     document.getElementById("contentCover").style.background = "white";
     await sleep(500);
     document.getElementById("pageContent").src = link;
     await sleep(500);
     document.getElementById("contentCover").style.background = "transparent";
+}
+
+function go(linktex) {
+    window.open(linktex, "_blank");
+    win.focus();
+}
+
+// FIXME
+function copyDisc() {
+    var el = document.createElement('textarea');
+    el.value = "Sir Code#4374";
+    el.setAttribute('readonly', '');
+    el.style = { position: 'absolute', left: '-9999px' };
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
 }
