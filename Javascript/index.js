c6947c42fe54f07090391ccc1a84f13bf9bb3058
Sir Code <44554976+skylerspark@users.noreply.github.com>
2019-11-15 07:25:53 -0500

    Update index.js

--- Javascript/index.js
+++ Javascript/index.js
@@ -21,17 +21,6 @@ function go(linktex) {
     window.focus();
 }
 
-function copyDisc() {
-    var el = document.createElement('textarea');
-    el.value = "Sir Code#4374";
-    el.setAttribute('readonly', '');
-    el.style = { position: 'absolute', left: '-9999px' };
-    document.body.appendChild(el);
-    el.select();
-    document.execCommand('copy');
-    document.body.removeChild(el);
-}
-
 window.onscroll = function() {myFunction()};
 
 var navbar = document.getElementById("header");
