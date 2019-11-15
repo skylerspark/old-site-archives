53550151c06f0b389b199b9ce0bac3e14e1a9810
Sir Code <44554976+skylerspark@users.noreply.github.com>
2019-11-15 07:33:01 -0500

    Update index.js

--- Javascript/index.js
+++ Javascript/index.js
@@ -20,16 +20,3 @@ function go(linktex) {
     window.open(linktex, "_blank");
     window.focus();
 }
-
-window.onscroll = function() {myFunction()};
-
-var navbar = document.getElementById("header");
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
