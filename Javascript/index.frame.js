adcb3fb6226d09be0b1b3e30bdd0f0b37134306c
Sir Code <44554976+skylerspark@users.noreply.github.com>
2019-11-15 07:26:06 -0500

    Update index.frame.js

--- Javascript/index.frame.js
+++ Javascript/index.frame.js
@@ -1,1 +1,15 @@
+function go(linktex) {
+    window.open(linktex, "_blank");
+    window.focus();
+}
 
+function copyDisc() {
+    var el = document.createElement('textarea');
+    el.value = "Sir Code#4374";
+    el.setAttribute('readonly', '');
+    el.style = { position: 'absolute', left: '-9999px' };
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+}
